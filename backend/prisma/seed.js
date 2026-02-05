22ce1c3a7a1d4f17859ed52acc6a64d907cd3d8e
DESKTOP-V5B6S54\DREAMWORLD <harshg.software@gmail.com>
2026-02-05 14:22:02 +0530

    seed: Add seed data in seed.js add unique constraint to membership in schema

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -1,90 +1,182 @@
+const bcrypt = require("bcryptjs");
 const prisma = require("../src/config/prisma");
 
+const orgId = "f89da722-1b24-47d8-8c62-a971a50a01b4";
+
 async function main() {
-    const permissions = [
-        "user:create",
-        "user:read",
-        "user:update",
-        "user:delete",
+    //organisation
+    const org = await prisma.organisation.upsert({
+        where: {
+            id: orgId,
+        },
+        update: {},
+        create: {
+            id: orgId,
+            name: "Acme Inc",
+        },
+    });
+
+    //user
+    const hashedAdminPassword = await bcrypt.hash("admin@123", 10);
+    const hashedUserPassword = await bcrypt.hash("user@123", 10);
+
+    const adminUser = await prisma.user.upsert({
+        where: {
+            email: "admin@acme.com",
+        },
+        update: {},
+        create: {
+            email: "admin@acme.com",
+            password: hashedAdminPassword,
+        },
+    });
+
+    const normalUser = await prisma.user.upsert({
+        where: {
+            email: "user@acme.com",
+        },
+        update: {},
+        create: {
+            email: "user@acme.com",
+            password: hashedUserPassword,
+        },
+    });
+
+    //roles
+    const adminRole = await prisma.role.upsert({
+        where: {
+            name_organisationId: {
+                name: "ADMIN",
+                organisationId: org.id,
+            },
+        },
+
+        update: {},
+        create: {
+            name: "ADMIN",
+            organisationId: org.id,
+        },
+    });
+
+    const memberRole = await prisma.role.upsert({
+        where: {
+            name_organisationId: {
+                name: "MEMBER",
+                organisationId: org.id,
+            },
+        },
+        update: {},
+        create: {
+            name: "MEMBER",
+            organisationId: org.id,
+        },
+    });
+
+    //permission
+    const permissionsData = [
+        { key: "user:create", organisationId: org.id },
+        { key: "user:read", organisationId: org.id },
+        { key: "project:create", organisationId: org.id },
+        { key: "project:read", organisationId: org.id },
     ];
 
     const permissionRecords = {};
-    for (const key of permissions) {
-        const permission = await prisma.permission.upsert({
-            where: { key },
-            update: {},
-            create: { key },
-        });
 
-        permissionRecords[key] = permission;
-    }
-
-    const adminRole = await prisma.role.upsert({
-        where: { name: "ADMIN" },
-        update: {},
-        create: { name: "ADMIN" },
-    });
-
-    const userRole = await prisma.role.upsert({
-        where: { name: "USER" },
-        update: {},
-        create: { name: "USER" },
-    });
-
-    // ADMIN -> ALL PERMISSION
-    for (const permission of Object.values(permissionRecords)) {
-        await prisma.rolePermission.upsert({
+    for (const permission of permissionsData) {
+        const record = await prisma.permission.upsert({
             where: {
-                roleId_permissionId: {
-                    roleId: adminRole.id,
-                    permissionId: permission.id,
+                key_organisationId: {
+                    key: permission.key,
+                    organisationId: permission.organisationId,
                 },
             },
             update: {},
             create: {
-                roleId: adminRole.id,
-                permissionId: permission.id,
+                key: permission.key,
+                organisationId: permission.organisationId,
             },
         });
+
+        permissionRecords[permission.key] = record;
     }
 
-    // USER -> READ ONLY
-    await prisma.rolePermission.upsert({
+    const allPermissions = Object.values(permissionRecords);
+
+    // role-permission
+    //admin
+    await Promise.all(
+        allPermissions.map((permission) =>
+            prisma.rolePermission.upsert({
+                where: {
+                    roleId_permissionId: {
+                        roleId: adminRole.id,
+                        permissionId: permission.id,
+                    },
+                },
+                update: {},
+                create: {
+                    roleId: adminRole.id,
+                    permissionId: permission.id,
+                },
+            }),
+        ),
+    );
+
+    //member
+    const readPermissions = allPermissions.filter((p) =>
+        p.key.endsWith(":read"),
+    );
+
+    await Promise.all(
+        readPermissions.map((permission) =>
+            prisma.rolePermission.upsert({
+                where: {
+                    roleId_permissionId: {
+                        roleId: memberRole.id,
+                        permissionId: permission.id,
+                    },
+                },
+                update: {},
+                create: {
+                    roleId: memberRole.id,
+                    permissionId: permission.id,
+                },
+            }),
+        ),
+    );
+
+    // membership
+    //admin
+    await prisma.memberShip.upsert({
         where: {
-            roleId_permissionId: {
-                roleId: userRole.id,
-                permissionId: permissionRecords["user:read"].id,
+            userId_organisationId_roleId: {
+                userId: adminUser.id,
+                organisationId: org.id,
+                roleId: adminRole.id,
             },
         },
         update: {},
         create: {
-            roleId: userRole.id,
-            permissionId: permissionRecords["user:read"].id,
+            userId: adminUser.id,
+            organisationId: org.id,
+            roleId: adminRole.id,
         },
     });
 
-    // TEST USER
-    const testUser = await prisma.user.upsert({
-        where: { email: "user@example.com" },
-        update: {},
-        create: {
-            email: "user@example.com",
-            password: "user@123", // hashed pass
-        },
-    });
-
-    // ASSIIGN USER ROLE
-    await prisma.userRole.upsert({
+    //member
+    await prisma.memberShip.upsert({
         where: {
-            userId_roleId: {
-                userId: testUser.id,
-                roleId: userRole.id,
+            userId_organisationId_roleId: {
+                userId: normalUser.id,
+                organisationId: org.id,
+                roleId: memberRole.id,
             },
         },
         update: {},
         create: {
-            userId: testUser.id,
-            roleId: userRole.id,
+            userId: normalUser.id,
+            organisationId: org.id,
+            roleId: memberRole.id,
         },
     });
 }
