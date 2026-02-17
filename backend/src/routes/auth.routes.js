bf0b9899243d7c14f70073e5b38826ffdcfa7c10
DESKTOP-V5B6S54\DREAMWORLD <harshg.software@gmail.com>
2026-02-17 15:55:25 +0530

    feat(auth): email verification - change schema - add feature to verify email after register - add reset password to authenticated user - add forgot passsword using email verification

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,6 +5,14 @@ const prisma = require("../config/prisma");
 const { hashPassword, verifyPassword } = require("../auth/password");
 const { authenticate } = require("../auth/authMiddleware");
 const { getEffectivePermissions } = require("../rbac/getEffectivePermissions");
+const {
+    sendVerificationEmail,
+    sendForgotPasswordEmail,
+} = require("../service/email.service");
+const {
+    createVerificationToken,
+    verifyToken,
+} = require("../service/token.service");
 
 const router = express.Router();
 
@@ -18,24 +26,70 @@ router.post("/register", async (req, res) => {
         },
     });
 
-    if (user) {
+    if (user && user.isActive) {
         return res.status(400).json({ message: "User is aldready registered" });
     }
 
     //hash password
-    const hashedPassword = await hashPassword(password);
+    const passwordHash = await hashPassword(password);
 
-    const newUser = await prisma.user.create({
-        data: {
+    const newUser = await prisma.user.upsert({
+        where: {
             email,
-            password: hashedPassword,
+        },
+        update: {
+            passwordHash,
+            isActive: false,
+        },
+        create: {
+            email,
+            passwordHash,
+            isActive: false,
         },
     });
 
+    const token = await createVerificationToken(newUser.id, "EMAIL_VERIFY");
+    await sendVerificationEmail(newUser.email, token);
+
     res.json({
-        message: "User registered",
-        user: newUser.email,
+        message: "Verification email sent",
+        email: newUser.email,
     });
+});
+
+//verify mail
+router.post("/verify-email", async (req, res) => {
+    const { email, token } = req.body;
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (!user) {
+        return res.status(400).json({ message: "User not found" });
+    }
+
+    try {
+        await verifyToken(user.id, "EMAIL_VERIFY", token);
+
+        await prisma.user.update({
+            where: {
+                id: user.id,
+            },
+            data: {
+                isActive: true,
+            },
+        });
+
+        res.json({
+            message: "Mail successfully verified",
+            email: user.email,
+        });
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
 });
 
 // login
@@ -58,118 +112,250 @@ router.post("/login", async (req, res) => {
         return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    const valid = await verifyPassword(password, user.password);
+    const valid = await verifyPassword(password, user.passwordHash);
     if (!valid) {
         return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    //if user has no organisation --> create default one
-    if (!user.memberShips.length) {
-        const organisation = await prisma.organisation.create({
-            data: {
-                name: `${email.split("@")[0]}'s Workspace`,
+    if (!user.isActive) {
+        const verificationToken = await prisma.verificationToken.findFirst({
+            where: {
+                userId: user.id,
+                type: "EMAIL_VERIFY",
             },
         });
 
-        //create owner role
-        const ownerRole = await prisma.role.create({
-            data: {
-                organisationId: organisation.id,
-                name: "OWNER",
-            },
-        });
+        if (!verificationToken || verificationToken.expiresAt < new Date()) {
+            const newToken = await createVerificationToken(
+                user.id,
+                "EMAIL_VERIFY",
+            );
 
-        //set of default permission
-        const defaultPermissions = [
-            "user:create",
-            "user:read",
-            "user:update",
-            "user:delete",
-            "role:create",
-            "role:read",
-            "role:update",
-            "role:delete",
-            "permission:create",
-            "permission:read",
-            "permission:update",
-            "permission:delete",
-        ];
-
-        for (const key of defaultPermissions) {
-            const permission = await prisma.permission.upsert({
-                where: {
-                    key_organisationId: {
-                        key,
-                        organisationId: organisation.id,
-                    },
-                },
-                update: {},
-                create: {
-                    key,
-                    organisationId: organisation.id,
-                },
-            });
-
-            await prisma.rolePermission.create({
-                data: {
-                    roleId: ownerRole.id,
-                    permissionId: permission.id,
-                },
+            await sendVerificationEmail(user.email, newToken);
+            return res.status(403).json({
+                message: "Verification expired. New email sent.",
             });
         }
 
-        // assign owner membership
-        await prisma.memberShip.create({
-            data: {
-                userId: user.id,
-                organisationId: organisation.id,
-                roleId: ownerRole.id,
-            },
-        });
-
-        user = await prisma.user.findUnique({
-            where: {
-                id: user.id,
-            },
-            include: {
-                memberShips: {
-                    include: {
-                        organisation: true,
-                        role: true,
-                    },
-                },
-            },
+        return res.status(403).json({
+            message: "Please verify your mail before logging in",
         });
     }
 
-    //pick default org (logic: first org)
-    const defaultOrg = user.memberShips[0].organisation;
+    // //if user has no organisation --> create default one
+    // if (!user.memberShips.length) {
+    //     const organisation = await prisma.organisation.create({
+    //         data: {
+    //             name: `${email.split("@")[0]}'s Workspace`,
+    //         },
+    //     });
+
+    //     //create owner role
+    //     const ownerRole = await prisma.role.create({
+    //         data: {
+    //             organisationId: organisation.id,
+    //             name: "OWNER",
+    //         },
+    //     });
+
+    //     //set of default permission
+    //     const defaultPermissions = [
+    //         "user:create",
+    //         "user:read",
+    //         "user:update",
+    //         "user:delete",
+    //         "role:create",
+    //         "role:read",
+    //         "role:update",
+    //         "role:delete",
+    //         "permission:create",
+    //         "permission:read",
+    //         "permission:update",
+    //         "permission:delete",
+    //     ];
+
+    //     for (const key of defaultPermissions) {
+    //         const permission = await prisma.permission.upsert({
+    //             where: {
+    //                 key_organisationId: {
+    //                     key,
+    //                     organisationId: organisation.id,
+    //                 },
+    //             },
+    //             update: {},
+    //             create: {
+    //                 key,
+    //                 organisationId: organisation.id,
+    //             },
+    //         });
+
+    //         await prisma.rolePermission.create({
+    //             data: {
+    //                 roleId: ownerRole.id,
+    //                 permissionId: permission.id,
+    //             },
+    //         });
+    //     }
+
+    //     // assign owner membership
+    //     await prisma.memberShip.create({
+    //         data: {
+    //             userId: user.id,
+    //             organisationId: organisation.id,
+    //             roleId: ownerRole.id,
+    //         },
+    //     });
+
+    //     user = await prisma.user.findUnique({
+    //         where: {
+    //             id: user.id,
+    //         },
+    //         include: {
+    //             memberShips: {
+    //                 include: {
+    //                     organisation: true,
+    //                     role: true,
+    //                 },
+    //             },
+    //         },
+    //     });
+    // }
+
+    // //pick default org (logic: first org)
+    // const defaultOrg = user.memberShips[0].organisation;
+
+    // const token = signToken({
+    //     userId: user.id,
+    //     organisationId: defaultOrg.id,
+    //     organisationName: defaultOrg.name,
+    // });
+
+    // const organisations = user.memberShips.map((m) => ({
+    //     organisationsId: m.organisation.id,
+    //     name: m.organisation.name,
+    //     role: m.role.name,
+    // }));
+
+    // const effectivePermissions = await getEffectivePermissions(
+    //     user.id,
+    //     defaultOrg.id,
+    // );
+
+    // res.json({
+    //     email: user.email,
+    //     token,
+    //     organisationId: defaultOrg.id,
+    //     organisationName: defaultOrg.name,
+    //     roles: effectivePermissions.roles,
+    //     permissions: effectivePermissions.permissions,
+    // });
 
     const token = signToken({
         userId: user.id,
-        organisationId: defaultOrg.id,
-        organisationName: defaultOrg.name,
     });
-
-    const organisations = user.memberShips.map((m) => ({
-        organisationsId: m.organisation.id,
-        name: m.organisation.name,
-        role: m.role.name,
-    }));
-
-    const effectivePermissions = await getEffectivePermissions(
-        user.id,
-        defaultOrg.id,
-    );
 
     res.json({
-        email: user.email,
+        user,
         token,
-        organisationId: defaultOrg.id,
-        organisationName: defaultOrg.name,
-        roles: effectivePermissions.roles,
-        permissions: effectivePermissions.permissions,
     });
+});
+
+//password reset using current password (logged in user)
+router.post("/reset-password", authenticate, async (req, res) => {
+    const { currentPassword, newPassword } = req.body;
+
+    const userId = req.user.id;
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+    });
+
+    const isValid = await verifyPassword(currentPassword, user.passwordHash);
+
+    if (!isValid) {
+        return res.status(400).json({
+            message: "Current password is incorrect",
+        });
+    }
+
+    const newPasswordHash = await hashPassword(newPassword);
+
+    await prisma.user.update({
+        where: {
+            id: user.id,
+        },
+        data: {
+            passwordHash: newPasswordHash,
+        },
+    });
+
+    res.json({ message: "Password successfully updated" });
+});
+
+//forgot password (using email verification)
+router.post("/forgot-password", async (req, res) => {
+    const { email } = req.body;
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (!user) {
+        return res.status(404).json({
+            message: "Account not exist",
+        });
+    }
+
+    const token = await createVerificationToken(user.id, "PASSWORD_RESET");
+
+    await sendForgotPasswordEmail(user.email, token);
+
+    return res.status(200).json({
+        message: "Reset OTP send to email",
+    });
+});
+
+//verify reset password token --> set new password
+router.post("/reset-password-token", async (req, res) => {
+    const { email, token, newPassword } = req.body;
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (!user) {
+        return res.status(404).json({
+            message: "Invalid request",
+        });
+    }
+
+    try {
+        await verifyToken(user.id, "PASSWORD_RESET", token);
+
+        const newPasswordHash = await hashPassword(newPassword);
+
+        await prisma.user.update({
+            where: {
+                id: user.id,
+            },
+            data: {
+                passwordHash: newPasswordHash,
+            },
+        });
+        res.json({
+            message: "Password successfully reset",
+        });
+    } catch (error) {
+        return res.status(400).json({
+            error: error.message,
+        });
+    }
 });
 
 // switch organisation
